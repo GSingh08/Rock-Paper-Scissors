41afbac6e7a49d4f541e57762861bd750bd74fe2
Gurjot Singh <gurjot@myerrunds.com>
2019-01-29 14:44:39 -0500

    Caching the DOM

---
+++ app.js
@@ -0,0 +1,14 @@
+//Declaring your variables in the start
+//You can also grab your elements (line 5-6) to use later, naming is important!
+//This is called Caching the DOM.
+//This means to store something for future use!
+//Every one of your projects should start like this.
+const userScore = 0;
+const computerScore=0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("comp-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = Document.getElementById("r");
+const paper_div = Document.getElementById("p");
+const scissors_div = Document.getElementById("s");
